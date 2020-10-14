6c214ed3474aadd78c56b4750f2921c67f177b5b
KelvinHao <nnhao9a3@gmail.com>
2020-10-14 10:10:20 +0700

    Add project files.

---
+++ eShopWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eShopWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
